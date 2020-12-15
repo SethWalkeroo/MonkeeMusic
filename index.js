1afb3e0cc37cabcaa26311afcf5841ec828b1d15
sethwalkeroo <sethrobertwalker@gmail.com>
2020-12-15 09:18:34 -0700

    added token to environment variable so I can stop caring about it.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require('fs')
 const Discord = require('discord.js');
 const Client = require('./client/Client');
-const { prefix, token } = require('./config.json');
+const prefix = require('./config.json');
+const env = require('dotenv').config();
+const token = process.env.TOKEN;
 
 const client = new Client();
 client.commands = new Discord.Collection();
