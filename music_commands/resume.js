c05eb0079138d9ce75a1e82c1d3a07fcc2ca09e5
sethwalkeroo <sethrobertwalker@gmail.com>
2020-12-16 12:26:28 -0700

    added pause and resume

--- music_commands/resume.js
+++ music_commands/resume.js
@@ -1,14 +1,15 @@
 module.exports = {
-	name: 'pause',
-	description: 'Pause the current song.',
-	usage: '#pause',
+	name: 'resume',
+	description: 'Resume the current song.',
+	usage: '#resume',
 	guildOnly: true,
 	cooldown: 2,
 	async execute(message) {
 		const serverQueue = message.client.queue.get(message.guild.id);
 		const player = serverQueue.connection.dispatcher;
 		if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to pause the music.');
-		await player.pause(true);
-		return message.channel.send('The bot has been paused! :monkey_face: :thumbup:');
+		await player.resume();
+		return message.channel.send('The bot has resumed playing music! :monkey_face: :thumbup:');
+
 	},
 };
