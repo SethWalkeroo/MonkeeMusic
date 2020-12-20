938701bffc93650403e3151b556f4a2d570fba36
sethwalkeroo <sethrobertwalker@gmail.com>
2020-12-20 14:58:30 -0700

    added try catch for joining before guild.voice exists

--- music_commands/join.js
+++ music_commands/join.js
@@ -8,7 +8,12 @@ module.exports = {
 	async execute(message) {
 		const voiceChannel = message.member.voice.channel;
 		if (!message.member.voice.channel) return await message.channel.send('You have to be in a voice channel for the bot to join.');
-		if (message.guild.voice.channel) return message.channel.send('The bot is already in a voice channel!');
+		try {
+			if (message.guild.voice.channel) return message.channel.send('The bot is already in a voice channel!');
+		} catch (error) {
+			console.log('Guild voice stuff doesnt exist yet. Safe to join.');
+		}
+
 		await voiceChannel.join();
 		if (!message.client.config.silent) {
 			await message.channel.send('The bot has joined the voice channel! :monkey_face:');
